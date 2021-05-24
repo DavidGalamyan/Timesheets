064ae6f210310c888f28f708127812bf7c8db739
DavidGalamyan <DavidGalamyan@gmail.com>
2021-05-25 01:38:05 +0300

    Реализовать контроллеры, User,Employee

--- Timesheets/Controllers/EmployeesController.cs
+++ Timesheets/Controllers/EmployeesController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Timesheets.Domain.Interfaces;
+using Timesheets.Models.Dto;
 
 namespace Timesheets.Controllers
 {
@@ -11,5 +10,40 @@ namespace Timesheets.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly IEmployeeManager _employeeManager;
+        public EmployeesController(IEmployeeManager employeeManager)
+        {
+            _employeeManager = employeeManager;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromQuery] Guid id)
+        {
+            var result = await _employeeManager.GetItem(id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] EmployeeRequest request)
+        {
+            var id = await _employeeManager.Create(request);
+            return Ok(id);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, EmployeeRequest request)
+        {
+            await _employeeManager.Update(id, request);
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _employeeManager.GetItems();
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromQuery] Guid id)
+        {
+            await _employeeManager.Delete(id);
+            return Ok();
+        }
     }
 }
--- Timesheets/Controllers/UsersController.cs
+++ Timesheets/Controllers/UsersController.cs
@@ -1,9 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Timesheets.Domain.Interfaces;
+using Timesheets.Models.Dto;
 
 namespace Timesheets.Controllers
 {
@@ -11,5 +10,41 @@ namespace Timesheets.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly IUserManager _userManager;
+
+        public UsersController(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get([FromQuery] Guid id)
+        {
+            var result = await _userManager.GetItem(id);
+            return Ok(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] UserRequest request)
+        {
+            var id = await _userManager.Create(request);
+            return Ok(id);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, UserRequest request)
+        {
+            await _userManager.Update(id, request);
+            return Ok();
+        }
+        [HttpGet]        
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _userManager.GetItems();
+            return Ok(result);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromQuery] Guid id)
+        {
+            await _userManager.Delete(id);
+            return Ok();
+        }
     }
 }
